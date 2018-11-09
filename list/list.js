b405a12620b3f50274c06ccc3770063fdf62bfd9
ibnzsubajr <gyokerrobi@yahoo.com>
2018-11-09 12:06:28 +0100

    hello

--- list/list.js
+++ list/list.js
@@ -3,8 +3,18 @@ function getData(url) {
     fetch(url).then(function (r) {
         return r.json();
     }).then(function (j) {
-        console.log(j);
+        listData(j);
     })
 }
 
 getData(url);
+
+function listData(data){
+    document.getElementById('name').innerHTML = data.name;
+    document.getElementById('manufacturer').innerHTML = data.manufacturer;
+    document.getElementById('price').innerHTML = data.price;
+    document.getElementById('available').innerHTML = data.available;
+    document.getElementById('img').innerHTML = data.img;
+}
+
+
