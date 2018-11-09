d8479dd31e3b5c17a042655e47ca767d3135f282
edem1993 <denesa1993@gmail.com>
2018-11-09 12:43:27 +0100

    666

--- cart/cart.js
+++ cart/cart.js
@@ -4,8 +4,14 @@ function getData(url) {
     fetch(url).then(function (r) {
         return r.json();
     }).then(function (j) {
-        console.log(j);
+        load(j);
     })
 }
 
 getData(url);
+
+document.getElementById('del').addEventListener('click', load);
+
+function load(data) {
+    
+}
